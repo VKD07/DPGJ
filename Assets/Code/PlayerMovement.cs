7f206245be830c4085591fc1703815186a1c40d9
Vince Delgado <116175769+VKD07@users.noreply.github.com>
2025-05-05 12:48:48 +0400

    Added camera follow

--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -14,6 +15,13 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float _pitchSpeed = 60f;
     [SerializeField] private float _maxPitchAngle = 90f;
 
+    [Header("Camera Settings")] 
+    [SerializeField] private CinemachineThirdPersonFollow _thirdPersonFollow;
+    [SerializeField] private Vector3 _cameraOffsetUp = new Vector3(1.32f, 4.91f, 1.24f);
+    [SerializeField] private Vector3 _cameraOffsetNeutral = new Vector3(1.32f, 1.01f, -3.90f);
+    [SerializeField] private Vector3 _cameraOffsetDown = new Vector3(1.32f, -3.32f, -2.14f);
+    [SerializeField] private float _cameraFollowSmoothing = 5f;
+
     [Header("Visual Settings")]
     [SerializeField] private Transform visuals;
     [SerializeField] private float _maxRollAngle = 25f;
@@ -23,17 +31,16 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector2 _moveInput;
     private Vector2 _lookInput;
-
-    private float _currentPitch = 0f;
-    private float _currentYaw = 0f;
-    private float _currentThrustTilt = 0f;
-    private float _currentRollVisual = 0f;
+    private float _currentPitch;
+    private float _currentYaw;
+    private float _currentThrustTilt;
+    private float _currentRollVisual;
+    private Vector3 _currentShoulderOffset;
 
     private void Update()
     {
         _moveInput = _move.action.ReadValue<Vector2>();
         _lookInput = _look.action.ReadValue<Vector2>();
-
         Move();
         Rotate();
         UpdateVisuals();
@@ -41,34 +48,32 @@ public class PlayerMovement : MonoBehaviour
 
     private void Move()
     {
-        Vector3 forwardMovement = transform.forward * _moveInput.y * _moveSpeed;
-        Vector3 strafeMovement = transform.right * _moveInput.x * _strafeSpeed;
-
-        Vector3 combined = (forwardMovement + strafeMovement) * Time.deltaTime;
-        transform.position += combined;
+        Vector3 movement = (transform.forward * _moveInput.y * _moveSpeed + transform.right * _moveInput.x * _strafeSpeed) * Time.deltaTime;
+        transform.position += movement;
     }
 
     private void Rotate()
     {
-        float pitchDelta = -_lookInput.y * _pitchSpeed * Time.deltaTime;
-        float yawDelta = _lookInput.x * _yawSpeed * Time.deltaTime;
+        _currentPitch = Mathf.Clamp(_currentPitch - _lookInput.y * _pitchSpeed * Time.deltaTime, -_maxPitchAngle, _maxPitchAngle);
+        _currentYaw += _lookInput.x * _yawSpeed * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(_currentPitch, _currentYaw, 0f);
 
-        _currentPitch = Mathf.Clamp(_currentPitch + pitchDelta, -_maxPitchAngle, _maxPitchAngle);
-        _currentYaw += yawDelta;
-
-        Quaternion newRotation = Quaternion.Euler(_currentPitch, _currentYaw, 0f);
-        transform.rotation = newRotation;
+        float normalizedPitch = _currentPitch / _maxPitchAngle;
+        Vector3 targetOffset = _currentPitch > 0
+            ? Vector3.Lerp(_cameraOffsetNeutral, _cameraOffsetUp, normalizedPitch)
+            : Vector3.Lerp(_cameraOffsetNeutral, _cameraOffsetDown, -normalizedPitch);
+        _currentShoulderOffset = Vector3.Lerp(_currentShoulderOffset, targetOffset, Time.deltaTime * _cameraFollowSmoothing);
+        _thirdPersonFollow.ShoulderOffset = _currentShoulderOffset;
     }
 
     private void UpdateVisuals()
     {
-        if (visuals == null) return;
+        if (!visuals) return;
 
         float targetRoll = -_lookInput.x * _maxRollAngle;
         _currentRollVisual = Mathf.Lerp(_currentRollVisual, targetRoll, Time.deltaTime * _rollSmoothing);
 
-        float targetTilt = _moveInput.y > 0f ? -_thrustTiltAngle :
-                           _moveInput.y < 0f ? _thrustTiltAngle : 0f;
+        float targetTilt = _moveInput.y > 0f ? -_thrustTiltAngle : _moveInput.y < 0f ? _thrustTiltAngle : 0f;
         _currentThrustTilt = Mathf.Lerp(_currentThrustTilt, targetTilt, Time.deltaTime * _thrustTiltSmoothing);
 
         visuals.localRotation = Quaternion.Euler(-_currentThrustTilt, 0f, _currentRollVisual);
