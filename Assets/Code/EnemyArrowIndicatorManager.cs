9c24352bad84bb92f93c5f0e9e7cb04787bc1c5e
Vince Delgado <116175769+VKD07@users.noreply.github.com>
2025-05-06 16:09:52 +0400

    Modified Arrow indicator script

--- Assets/Code/EnemyArrowIndicatorManager.cs
+++ Assets/Code/EnemyArrowIndicatorManager.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 namespace Code
@@ -11,14 +9,11 @@ namespace Code
         [SerializeField] private Transform _player;
 
         private DroneSpawner _droneSpawner;
-
-        private readonly Dictionary<Transform, RectTransform> _enemiesToTrack =
-            new Dictionary<Transform, RectTransform>();
-
+        private Dictionary<Transform, RectTransform> _trackedArrows = new();
 
         private void OnEnable()
         {
-            _droneSpawner = FindAnyObjectByType<DroneSpawner>(); //Not satisfied
+            _droneSpawner = FindAnyObjectByType<DroneSpawner>();
         }
 
         private void Start()
@@ -28,51 +23,80 @@ namespace Code
 
         private void Update()
         {
-            TrackAllEnemiesWithArrow();
-        }
-
-        private void TrackAllEnemiesWithArrow()
-        {
-            foreach (var enemy in _enemiesToTrack)
-            {
-                if (enemy.Key.gameObject.activeSelf)
-                {
-                    TrackEnemyWithArrow(enemy.Key, enemy.Value);
-                    return;
-                }
-
-                enemy.Value.gameObject.SetActive(false);
-            }
-        }
-
-        private void TrackEnemyWithArrow(Transform toTrack, RectTransform arrowUI)
-        {
-            if (!_player) return;
-
-            arrowUI.gameObject.SetActive(true);
-
-            Vector3 enemy = toTrack.position - _player.position;
-            Vector3 forward = _player.forward;
-            Vector3 right = _player.right;
-
-            float x = Vector3.Dot(enemy, right);
-            float y = Vector3.Dot(enemy, forward);
-            float angle = Mathf.Atan2(x, y) * Mathf.Rad2Deg;
-
-            arrowUI.localEulerAngles = new Vector3(0, 0, -angle);
+            UpdateTrackedEnemies();
         }
 
         private void InitProducts()
         {
-            for (int i = 0; i < Products.Count; i++)
+            foreach (GameObject product in Products)
             {
-                Products[i].transform.SetParent(_uiParent);
-                Products[i].GetComponent<RectTransform>().position = Vector3.zero;
-                Products[i].gameObject.SetActive(false);
-
-                //Creating a dictionary to assign each enemy with their own arrow ui
-                _enemiesToTrack.Add(_droneSpawner.Products[i].transform, Products[i].GetComponent<RectTransform>());
+                product.transform.SetParent(_uiParent, false);
+                RectTransform rect = product.GetComponent<RectTransform>();
+                rect.anchoredPosition = Vector2.zero;
+                product.SetActive(false);
             }
+        }
+
+        private void UpdateTrackedEnemies()
+        {
+            if (_droneSpawner == null || _player == null) return;
+
+            foreach (var enemy in _droneSpawner.Products)
+            {
+                if (enemy == null || !enemy.gameObject.activeSelf) continue;
+
+                if (!_trackedArrows.ContainsKey(enemy.transform))
+                {
+                    var arrow = GetInactiveArrowFromPool();
+                    if (arrow != null)
+                    {
+                        _trackedArrows.Add(enemy.transform, arrow);
+                    }
+                }
+
+                if (_trackedArrows.TryGetValue(enemy.transform, out RectTransform arrowUI))
+                {
+                    TrackEnemyWithArrow(enemy.transform, arrowUI);
+                }
+            }
+
+            List<Transform> toRemove = new();
+            foreach (var kvp in _trackedArrows)
+            {
+                if (kvp.Key == null || !kvp.Key.gameObject.activeSelf)
+                {
+                    kvp.Value.gameObject.SetActive(false);
+                    toRemove.Add(kvp.Key);
+                }
+            }
+            foreach (var dead in toRemove)
+                _trackedArrows.Remove(dead);
+        }
+
+        private RectTransform GetInactiveArrowFromPool()
+        {
+            foreach (var obj in Products)
+            {
+                if (!obj.activeSelf)
+                {
+                    obj.SetActive(true);
+                    return obj.GetComponent<RectTransform>();
+                }
+            }
+            return null;
+        }
+
+        private void TrackEnemyWithArrow(Transform toTrack, RectTransform arrowUI)
+        {
+            Vector3 toEnemy = toTrack.position - _player.position;
+            Vector3 forward = _player.forward;
+            Vector3 right = _player.right;
+
+            float x = Vector3.Dot(toEnemy, right);
+            float y = Vector3.Dot(toEnemy, forward);
+            float angle = Mathf.Atan2(x, y) * Mathf.Rad2Deg;
+
+            arrowUI.localEulerAngles = new Vector3(0, 0, -angle);
         }
     }
 }
